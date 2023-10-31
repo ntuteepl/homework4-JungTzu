41eace8c01904160aa77a04f1ab1819eb00e8095
JungTzu <146064914+JungTzu@users.noreply.github.com>
2023-10-31 12:10:22 +0800

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d", &n);
     int feelings[10][10];
 
-    // 读取好感度矩阵
+    // 讀取好感度矩陣
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             scanf("%d", &feelings[i][j]);
@@ -15,16 +15,18 @@ int main() {
     int boys_matched[10] = {0};
     int girls_matched[10] = {0};
 
-    // 寻找最佳情侣组合
-    for (int i = 0; i < n; i++) {
+    // 尋找最佳情侶組合
+    for (int k = 0; k < n; k++) {
         int max = -1;
         int boy, girl;
 
-        for (int j = 0; j < n; j++) {
-            if (!girls_matched[j] && feelings[i][j] > max) {
-                max = feelings[i][j];
-                boy = i + 1;
-                girl = j + 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!boys_matched[i] && !girls_matched[j] && feelings[i][j] > max) {
+                    max = feelings[i][j];
+                    boy = i + 1;
+                    girl = j + 1;
+                }
             }
         }
 
